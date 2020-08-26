6f37a3c241448067c1e59e9eb3d535aba8ad8614
15951595 <shun19871001@gmail.com>
2020-08-26 21:26:31 +0900

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hellow_world/header/header_view.dart';
 import 'package:hellow_world/navigation_bar/navigation_bar_view.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,6 +29,7 @@ class PortfolioView extends StatelessWidget {
     print(height);
     print(width);
     return Scaffold(
+      drawer: DrawerView(),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -37,6 +39,36 @@ class PortfolioView extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class DrawerView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveBuilder(
+      builder: (_, size) {
+        if (!size.isMobile) return SizedBox();
+        return Drawer(
+            child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text("Item 1"),
+              trailing: Icon(Icons.arrow_forward),
+            ),
+            ListTile(
+              title: Text("Item 2"),
+              trailing: Icon(Icons.arrow_forward),
+            ),
+          ],
+        ));
+      },
     );
   }
 }
