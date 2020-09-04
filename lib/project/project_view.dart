0a6ca2841c97557ffd1158af0d3342c42c176f68
15951595 <shun19871001@gmail.com>
2020-09-04 22:35:20 +0900

    sizebox add

--- lib/project/project_view.dart
+++ lib/project/project_view.dart
@@ -60,47 +60,69 @@ class ProjectDesktopView extends StatelessWidget {
             SizedBox(height: 20),
             Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
               for (final item in kProjectItems)
-                Expanded(
-                    child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.asset(item.image),
-                      SizedBox(height: 15),
-                      Text(item.title,
-                          style: Theme.of(context).textTheme.headline4),
-                      SizedBox(height: 10),
-                      Text(
-                        item.description,
-                        style: TextStyle(fontSize: 17),
-                      ),
-                      SizedBox(height: 10),
-                      Row(
-                        children: [
-                          for (final tech in item.technologies)
-                            Padding(
-                              padding: const EdgeInsets.only(right: 8.0),
-                              child: Chip(
-                                  label: Text(tech,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headline4)),
-                            )
-                        ],
-                      )
-                    ],
-                  ),
-                ))
+                Expanded(child: ProjectItemBody(item: item))
             ])
           ],
         ));
   }
 }
 
+class ProjectItemBody extends StatelessWidget {
+  const ProjectItemBody({
+    Key key,
+    @required this.item,
+  }) : super(key: key);
+
+  final ProjectItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset(item.image),
+        SizedBox(height: 15),
+        Text(item.title, style: Theme.of(context).textTheme.headline4),
+        SizedBox(height: 10),
+        Text(
+          item.description,
+          style: TextStyle(fontSize: 17),
+        ),
+        SizedBox(height: 10),
+        Row(
+          children: [
+            for (final tech in item.technologies)
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Chip(
+                    label: Text(tech,
+                        style: Theme.of(context).textTheme.headline4)),
+              )
+          ],
+        ),
+        SizedBox(height: 50),
+      ],
+    );
+  }
+}
+
 class ProjectMobileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: kInitWidth,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Divider(
+              thickness: 3,
+            ),
+            SizedBox(height: 40),
+            Text('aa', style: Theme.of(context).textTheme.headline2),
+            SizedBox(height: 40),
+            for (final item in kProjectItems) ProjectItemBody(item: item)
+          ],
+        ));
   }
 }
